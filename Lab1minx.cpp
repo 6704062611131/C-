f2f29ec757c24e2f948bcedea3b4231a0c4ff28a
6704062611131 <s6704062611131@email.kmutnb.ac.th>
2024-11-26 15:02:54 +0700

    Add files via upload

--- Lab1minx.cpp
+++ Lab1minx.cpp
@@ -1,10 +1,20 @@
- #include<stdio.h>
- int main(){
- 	int a=50,b=30;
- 	
- 	if(a>b){
- 		printf("a is greater than b");
-	 }else{
-	 	printf("b is greater than a");
-	 }
- }
+#include<stdio.h>
+
+int main(){
+	int a,b;
+	printf("N1 : ");
+	scanf("%d", &a);
+	printf("N2 : ");
+	scanf("%d", &b);
+	
+	if(a>b){
+		printf("N1 %d \n",(a-b));
+	}else if(b>a){
+		printf("N2 %d \n",(b-a));
+	}else{
+		printf("N1=N2");
+	}
+}
+
+
+
